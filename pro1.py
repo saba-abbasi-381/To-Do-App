4e9b7c9ae3eac72293e271303d565445af836fa9
Saba Abbasi <abbasisaba745@gmail.com>
2026-03-30 14:18:55 +0500

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,53 @@
+class ToDo:
+    
+    def __init__(self):
+        self.task_lst = self.load_task()
+    
+    def save_task(self):
+        with open("task.txt", "w") as f:
+            for t in self.task_lst:
+                f.write(t + "\n")
+    
+    def load_task(self):
+        try:
+            with open("task.txt", "r") as f:
+                return f.read().splitlines()
+        except:
+            return []
+    
+    def add_task(self, task):
+        self.task_lst.append(task)
+        self.save_task()
+    
+    def view_task(self):
+        if not self.task_lst:
+            return "Task not exist!"
+        
+        tasks = []
+        for i, task in enumerate(self.task_lst, 1):
+            tasks.append(f"{i}.{task}")
+        return "\n".join(tasks)
+        
+        
+    def update_task(self, index , new_task):
+        self.view_task()
+        self.task_lst[index -1] = new_task
+        self.save_task()
+    
+    def delete_task(self, index):
+        self.view_task()
+        self.task_lst.pop(index - 1)
+        self.save_task()
+    
+    def search_task(self, task):
+        if task in self.task_lst: 
+            return f"Found: {task}"
+        else:
+            return "Sorry task not exist!"
+    
+    def clear_all(self):
+        self.task_lst.clear()    
+        self.save_task()
+
+
+
---
+++ pro1_App.py
@@ -0,0 +1,59 @@
+from pro1 import ToDo
+import streamlit as st
+
+todo = ToDo()
+
+st.title("To-Do App")
+st.header("Manage your daily task")
+
+st.sidebar.title("Menu")
+choice = st.sidebar.selectbox("Choose option", options=["Add", "View", "Update", "Search", "Delete", "Clear"])
+
+if choice == "Add":
+    task = st.text_input("enter task : ").lower()
+    if st.button("Add Task"):
+        if task:
+            todo.add_task(task)
+            st.success("Task added successfully!")
+        else:
+            st.warning("Please enter a task")
+
+elif choice == "View":
+    if st.button("View task"):
+        view_task = todo.view_task()
+        st.write(f"Your task: {view_task}")
+
+elif choice == "Update":
+    st.write("Current Tasks:")
+    st.text(todo.view_task())
+    index = st.number_input("select task number for updating: ",min_value = 1, step=1)
+    new_task = st.text_input("enter new task: ").lower()
+    if st.button("Update task"):
+        try:
+            todo.update_task(index, new_task)
+            st.success("Task updated!")
+        except:
+            st.error("Invalid task number")
+
+elif choice == "Delete":
+    st.write("Current Tasks:")
+    st.text(todo.view_task())
+    index = st.number_input("Select task number: ", min_value=1, step=1)
+    if st.button("Delete Task"):
+        try:
+            todo.delete_task(index)
+            st.success("Task deleted successfully")
+        except:
+            st.warning("Invalid task number")
+
+elif choice == "Search":
+    task = st.text_input("Enter Task name for searching: ").lower()
+    if st.button("Search Task"):
+        result = todo.search_task(task)
+        st.write(result)
+
+elif choice == "Clear":
+    if st.button("Clear task"):
+        todo.clear_all()
+        st.success("Task remove successfully")
+
